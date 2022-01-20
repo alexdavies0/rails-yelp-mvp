9f74bed721c2698b6788a5f0d7e3d3f0c5805703
Alex Davies <alxdavies@googlemail.com>
2022-01-20 19:20:11 +0000

    controllers created and routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get    "restaurants",                 to: "restaurants#index",  as: :restaurants
-  get    "restaurants/new",             to: "restaurants#new",    as: :new_restaurant
-  post   "restaurants",                 to: "restaurants#create"
-  get    "restaurants/:id",             to: "restaurants#show",   as: :restaurant
-  get    "restaurants/:id/reviews/new", to: "restaurants#new_review",   as: :new_review
-  post   "restaurants/:id/reviews/new", to: "restaurants#create_review"
+  # get    "restaurants",                 to: "restaurants#index",  as: :restaurants
+  # get    "restaurants/new",             to: "restaurants#new",    as: :new_restaurant
+  # post   "restaurants",                 to: "restaurants#create"
+  # get    "restaurants/:id",             to: "restaurants#show",   as: :restaurant
+  # get    "restaurants/:id/reviews/new", to: "restaurants#new_review",   as: :new_review
+  # post   "restaurants/:id/reviews/new", to: "restaurants#create_review"
+  resources :restaurants do
+    resources :reviews, only: %i[new create]
+  end
 end
